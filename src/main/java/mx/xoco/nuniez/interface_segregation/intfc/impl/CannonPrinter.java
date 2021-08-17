2c5e20cd55bad1a5b2bbca1e822c623f925f81b1
lentiummmx <lentium.mmx@gmail.com>
2021-08-17 17:46:34 -0500

    solid_principles_recap File(s) committed to adding interface segregation principle example (v0.4.0)

---
+++ src/main/java/mx/xoco/nuniez/interface_segregation/intfc/impl/CannonPrinter.java
@@ -0,0 +1,41 @@
+package mx.xoco.nuniez.interface_segregation.intfc.impl;
+
+import mx.xoco.nuniez.interface_segregation.intfc.IMultiFunction;
+
+public class CannonPrinter implements IMultiFunction {
+    @Override
+    public void print() {
+        // Real printing code starts here
+        //
+        //
+        // Real printing code ends here
+    }
+
+    @Override
+    public void getPrintSpoolDetails() {
+        // Real get print spool details code starts here
+        //
+        //
+        // Real get print spool details code ends here
+    }
+
+    @Override
+    public void scan() {
+
+    }
+
+    @Override
+    public void scanPhoto() {
+
+    }
+
+    @Override
+    public void fax() {
+
+    }
+
+    @Override
+    public void internetFax() {
+
+    }
+}
---
+++ src/main/java/mx/xoco/nuniez/interface_segregation/intfc/impl/HPPrinterNScanner.java
@@ -0,0 +1,47 @@
+package mx.xoco.nuniez.interface_segregation.intfc.impl;
+
+import mx.xoco.nuniez.interface_segregation.intfc.IMultiFunction;
+
+public class HPPrinterNScanner implements IMultiFunction {
+    @Override
+    public void print() {
+        // Real printing code starts here
+        //
+        //
+        // Real printing code ends here
+    }
+
+    @Override
+    public void getPrintSpoolDetails() {
+        // Real get print spool details code starts here
+        //
+        //
+        // Real get print spool details code ends here
+    }
+
+    @Override
+    public void scan() {
+        // Real scan code starts here
+        //
+        //
+        // Real scan code ends here
+    }
+
+    @Override
+    public void scanPhoto() {
+        // Real photo scan code starts here
+        //
+        //
+        // Real photo scan code ends here
+    }
+
+    @Override
+    public void fax() {
+
+    }
+
+    @Override
+    public void internetFax() {
+
+    }
+}
